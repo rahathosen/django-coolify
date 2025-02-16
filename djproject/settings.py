4dd004535dc22d5c4a657ffee1a5e667aa505171
rahathosen <rahathosen.me@gmail.com>
2025-02-16 12:27:34 +0600

    update CSRF_TRUSTED_ORIGINS to include additional trusted origin

--- djproject/settings.py
+++ djproject/settings.py
@@ -13,7 +13,11 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda x: [i.strip() for i in x.split(',')])
 
-CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://localhost:8000,http://127.0.0.1:8000', cast=lambda x: [i.strip() for i in x.split(',')])
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost:8000,http://127.0.0.1:8000,https://z4wksk8k8wwc0csg4400cgok.103.198.132.96.sslip.io',
+    cast=lambda x: [i.strip() for i in x.split(',')]
+)
 
 # Application definition
 
