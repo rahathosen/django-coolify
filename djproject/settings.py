9bd6d6b2f7c7bdbedc6d688e7ab45fa9d64eeeaf
rahathosen <rahathosen.me@gmail.com>
2025-02-16 13:01:21 +0600

    refactor DATABASES configuration to use dictionary format for SQLite

--- djproject/settings.py
+++ djproject/settings.py
@@ -75,12 +75,10 @@ WSGI_APPLICATION = 'djproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR}/db.sqlite3",
-        engine="django.db.backends.sqlite3",  
-        conn_max_age=600,
-        ssl_require=False,
-    ),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
